13135f281f594215432ab2ceb41d4e3ba4a9b569
furaizi <furachannel16@gmail.com>
2025-09-17 23:31:12 +0300

    Lab1: add CommandLineRunner implementation

--- Lab1/src/main/kotlin/org/example/lab1/Lab1Application.kt
+++ Lab1/src/main/kotlin/org/example/lab1/Lab1Application.kt
@@ -1,10 +1,18 @@
 package org.example.lab1
 
+import mu.KotlinLogging
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+private val log = KotlinLogging.logger {}
+
 @SpringBootApplication
-class Lab1Application
+class Lab1Application : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        log.info("Hello from Spring Boot!")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Lab1Application>(*args)
