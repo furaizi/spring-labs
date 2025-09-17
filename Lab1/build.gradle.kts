ef41c0cb4553dc1c11eb702a272d5993e628c7c5
furaizi <furachannel16@gmail.com>
2025-09-17 23:33:35 +0300

    Lab1: add Spring Web and DevTools dependencies

--- Lab1/build.gradle.kts
+++ Lab1/build.gradle.kts
@@ -21,8 +21,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
