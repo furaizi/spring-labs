5d0e15310f9bb373df1bbbaa6ae6fbae21cb3b2e
furaizi <furachannel16@gmail.com>
2025-10-20 19:39:52 +0300

    Create Topic entity

---
+++ Lab2/src/main/java/org/example/lab2/entity/Topic.java
@@ -0,0 +1,23 @@
+package org.example.lab2.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+public class Topic {
+    private Long id;
+    private String title;
+    private String description;
+    private String author;
+    private Integer viewCount;
+    private Integer replyCount;
+    private Boolean pinned;
+    private Boolean closed;
+    private Set<String> tags;
+    private Boolean deleted;
+    private LocalDateTime lastPostAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
