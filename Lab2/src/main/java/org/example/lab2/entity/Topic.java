5bff5bd667360adf101c86629b05a155a5607aad
furaizi <furachannel16@gmail.com>
2025-10-20 20:08:01 +0300

    Add fake Topic repository implementation

--- Lab2/src/main/java/org/example/lab2/entity/Topic.java
+++ Lab2/src/main/java/org/example/lab2/entity/Topic.java
@@ -4,10 +4,11 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.UUID;
 
 @Data
 public class Topic {
-    private String id;
+    private UUID id;
     private String title;
     private String description;
     private String author;
--- Lab2/src/main/java/org/example/lab2/repository/TopicRepository.java
+++ Lab2/src/main/java/org/example/lab2/repository/TopicRepository.java
@@ -4,6 +4,7 @@ import org.example.lab2.entity.Topic;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface TopicRepository {
     List<Topic> findAll();
@@ -13,18 +14,18 @@ public interface TopicRepository {
     List<Topic> findAllByTitle(String title);
     List<Topic> findAllByAuthor(String author);
 
-    Optional<Topic> findById(Long id);
+    Optional<Topic> findById(UUID id);
 
-    boolean existsById(Long id);
+    boolean existsById(UUID id);
 
     Topic save(Topic topic);
     List<Topic> saveAll(List<Topic> topics);
 
     Topic update(Topic topic);
 
-    int deleteById(Long id);
+    int deleteById(UUID id);
     int delete(Topic topic);
-    int deleteAllByIds(List<Long> ids);
+    int deleteAllByIds(List<UUID> ids);
     int deleteAll(List<Topic> topics);
     int deleteAll();
 }
