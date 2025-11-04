2d234a26035250464bbb2a836ac38ca09ccfb57b
furaizi <furachannel16@gmail.com>
2025-11-04 20:43:42 +0200

    Lab3: initialize project template

---
+++ Lab4/src/main/java/org/example/lab4/dto/TopicForm.java
@@ -0,0 +1,9 @@
+package org.example.lab4.dto;
+
+public class TopicForm {
+    private String title;
+    private String description;
+    private boolean pinned;
+    private boolean closed;
+    private String tags;
+}
---
+++ Lab4/src/main/java/org/example/lab4/service/PostService.java
@@ -0,0 +1,57 @@
+package org.example.lab4.service;
+
+import org.example.lab4.entity.Post;
+import org.example.lab4.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> getPostsByTopicId(UUID id) {
+        return postRepository.findByTopicId(id);
+    }
+
+    public List<Post> getPostsByAuthorId(UUID id) {
+        return postRepository.findByAuthorId(id);
+    }
+
+    public Optional<Post> getPostById(UUID id) {
+        return postRepository.findById(id);
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public boolean deletePost(UUID id) {
+        return postRepository.deleteById(id);
+    }
+
+    public List<Post> getPostByTitle(String keyword) {
+        return postRepository.findByTitleContaining(keyword);
+    }
+
+    public boolean likePost(UUID id) {
+        return postRepository.incrementLikes(id);
+    }
+
+    public boolean unlikePost(UUID id) {
+        return postRepository.decrementLikes(id);
+    }
+
+    public boolean updatePost(UUID id, String newTitle, String newContent) {
+        return postRepository.update(id, newTitle, newContent);
+    }
+}
